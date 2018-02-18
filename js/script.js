f65120fa4bfa21c0f9323b363712b4cc2103efed
“BabaSar” <Baba.Sariffodeen.1@gmail.com>
2018-02-18 21:15:45 +0000

    Started work on script.js
    Added functions to begin logic for the functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
+
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-//An array of object literals. Each object literal contains quotes and source information.
+//An array of object literals. Each object literal contains quotes and associated info
 var quotes = [
     {   quote: "First quote",
         source: "First source",
@@ -34,3 +35,48 @@ var quotes = [
         year: "Fifth year"
     }
 ];
+
+//first let's figure out how many quotes there are in the quotes array
+//If more quotes are added, then this can be determined dynamically
+var numOfQuotes = quotes.length;
+
+var previousGeneratedRandomNumber = null;
+
+//A function to generate a random number
+function getRandomNumber(upper){
+    var randomNumber = Math.floor(Math.random()*upper);
+    return randomNumber;
+}
+
+//A function called getRandomQuote. This will select a random quote from the quotes array.
+function getRandomQuote(){
+
+    var randNum = getRandomNumber(numOfQuotes);
+    //previousGeneratedRandomNumber = randNum;
+
+    //if the newly generated random number was the same as last time, get a different number
+    if (randNum !== previousGeneratedRandomNumber){
+        //using the randNum obtained, access this index position in the array to retrieve the quote object
+        var quote = quotes[randNum];
+
+        //log the quote to test if it works
+        console.log(quote);
+
+        //output the quote to the browser HTML
+        //some properties won't exist, how do we check for this?
+        var messageHTML = "";
+
+        previousGeneratedRandomNumber = randNum;
+
+    }else{
+        getRandomQuote();
+    }
+
+}
+
+//Declaring the printQuote function
+function printQuote(){
+    //calls the getRandomQuote() function, then stores returned object into a variable
+    getRandomQuote();
+    //construct the string containing the different quote properties using HTML template
+}
